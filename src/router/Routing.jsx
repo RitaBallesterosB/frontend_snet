3ef74dab62856120c773032bf47f75b9d3e8893e
RitaBB <ritaballesterosba@gmail.com>
2024-09-03 07:35:26 -0500

    create Sidebar and Feed components and update Routing.jsx

--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -2,22 +2,27 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PublicLayout } from "../components/layouts/public/PublicLayout";
 import { Login } from "../components/user/Login";
 import { Register } from "../components/user/Register";
+import { PrivateLayout } from "../components/layouts/private/PrivateLayout";
+import { Feed } from "../components/publication/Feed";
 
 export const Routing = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Cargar los componenetes de la ruta pública */}
+        {/* Cargar los componentes de la ruta pública */}
           <Route path='/' element={<PublicLayout />}>
             <Route index element={<Login />}/>
             <Route path='login' element={<Login />}/>
             <Route path='registro' element={<Register />}/>
           </Route>
-        
+
         {/* Cargar los componentes de la ruta privada */}
+          <Route path='/rsocial' element={<PrivateLayout />}>
+            <Route index element={<Feed />}/>
+            <Route path='feed' element={<Feed />}/>
+          </Route>
 
       </Routes>
     </BrowserRouter>
   )
 }
-
